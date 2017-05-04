73a5d5751552602e969f9ad5eb49693c380a033e
Christian Martinez <cgmartinez11142@gmail.com>
2017-05-04 09:22:23 -0700

    add initial files

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:`
+  <div class="jumbotron">
+    <h1>Tap Room</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
